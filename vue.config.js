51bad7207cd5bbc6b9cc578ed5ecfe29c383c32d
Shamil Magomedov <shomainkognito@yandex.ru>
2024-02-15 14:20:10 +0300

    #fix config production link

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/masha-and-bear/' : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
